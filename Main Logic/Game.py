5a7ba4dbf70ba1ccf2471719c040250925923661
Anisha Jain <anisha.j@misb.ac.th>
2024-10-05 13:30:18 +0700

    [feat] python code for asking user to input a single alphabet and storing it in a variable called letter

--- Main Logic/Game.py
+++ Main Logic/Game.py
@@ -1,1 +1,12 @@
-print("hello world")
+Secret="Anisha"
+def entersinglechar():
+    while True:
+        UserInput = input("Enter a letter:")
+        if len(UserInput) == 1 and UserInput.isalpha():
+            return(UserInput)
+        else:
+            print("invalid input. Please enter a single alphabet")
+
+letter= entersinglechar()
+
+
