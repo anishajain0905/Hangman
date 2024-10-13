382bfd8ffe09d9a6d3d97d638efcf00689a236d6
Anisha Jain <anisha.j@misb.ac.th>
2024-10-13 19:29:49 +0700

    [Fix] creating a new function called validate_user_guess instead of it being in track past guess

--- Main Logic/Game.py
+++ Main Logic/Game.py
@@ -1,32 +1,47 @@
 word_to_guess="anisha"
-print(len(word_to_guess) *"_")
+guessed_word= list("_" * len(word_to_guess))
+guessed_word_normal=" "
+print(guessed_word_normal.join(guessed_word))
 def enter_single_char():
+    userinput = input("Enter a letter:").lower()
     while True:
-        userinput = input("Enter a letter:").lower()
         if len(userinput) == 1 and userinput.isalpha():
             return userinput
         else:
-            print("invalid input. Please enter a single alphabet")
+            userinput = input("invalid input. Please enter a single alphabet")
 
 user_guess= enter_single_char()
+def validate_user_guess(guessed_word, word_to_guess):
+    if user_guess in word_to_guess:
+        for i in range (len(word_to_guess)):
+            if word_to_guess[i] == user_guess:
+                guessed_word[i] = user_guess
+                return guessed_word
+    else:
+        print("Sorry, Wrong guess!")
+        return []
 
+'''def singlegameloop
+word_to_guess="anisha"
+print(len(word_to_guess) *"_")
+def multigameloop
+
+def getrandomword
+    
+def set'''
+
+guessed_word= "_" * len(word_to_guess)
 def track_past_guesses():
     past_guess= []
     attempts = 7
     guessed_word= "_" * len(word_to_guess)
     while attempts > 0 :
+        past_guess= [user_guess]
         print("Number of Attempts remaining:",attempts)
         enter_single_char()
-        past_guess= [user_guess]
-        if user_guess in word_to_guess:
-             for i in len(word_to_guess):
-                 if word_to_guess[i] == user_guess:
-                     guessed_word[i] = user_guess
-                     return guessed_word
-        else:
-            print("Sorry, Wrong guess!")
-            attempts= (attempts - 1)
+        validate_user_guess(guessed_word, word_to_guess)
 
+track_past_guesses()
 
 
 
