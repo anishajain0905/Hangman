f9918ef19a2fc333bf4dba818fa41fdc53aa65c0
Anisha Jain <anisha.j@misb.ac.th>
2024-10-22 23:05:10 +0700

    [feat] adding users guesses to past guesses and reducing attempts to add flow to the game.

--- Main Logic/Game.py
+++ Main Logic/Game.py
@@ -1,45 +1,52 @@
 word_to_guess="anisha"
-guessed_word= list("_" * len(word_to_guess))
-guessed_word_normal=" "
-print(guessed_word_normal.join(guessed_word))
+guessed_word= ["_"] * len(word_to_guess)
+attempts = 7
+past_guess = []
+
 def enter_single_char():
     userinput = input("Enter a letter:").lower()
     while True:
         if len(userinput) == 1 and userinput.isalpha():
             return userinput
         else:
-            userinput = input("invalid input. Please enter a single alphabet")
+            userinput = input("Invalid input. Please enter a single alphabet:").lower()
 
-user_guess= enter_single_char()
-def validate_user_guess(guessed_word, word_to_guess):
+def validate_user_guess(guessed_word, word_to_guess, attempts, user_guess):
+    print(guessed_word)
+    word_to_guess2= list(word_to_guess)
     if user_guess in word_to_guess:
-        for i in range (len(word_to_guess)):
-            if word_to_guess[i] == user_guess:
+        for i in range(len(word_to_guess)):
+            if word_to_guess2[i] == user_guess:
                 guessed_word[i] = user_guess
-                return guessed_word
+        return guessed_word,attempts
     else:
-        print("Sorry, Wrong guess!")
-        return []
-
-'''def singlegameloop
-word_to_guess="anisha"
-print(len(word_to_guess) *"_")
+        attempts -= 1
+        print(f"Sorry Wrong Guess! Attempts remaining:{attempts}")
+        return guessed_word,attempts
+'''
 def multigameloop
+def getrandomword'''
 
-def getrandomword
-    
-def set'''
-
-guessed_word= "_" * len(word_to_guess)
 def track_past_guesses():
-    past_guess= []
-    attempts = 7
-    guessed_word= "_" * len(word_to_guess)
-    while attempts > 0 :
-        past_guess= [user_guess]
+    global attempts
+    global guessed_word
+    while attempts > 0 and "_" in guessed_word:
+        print(f"Word to guess:{''.join(guessed_word)}")
+        print(f"Past guesses:{','.join(past_guess)}")
         print("Number of Attempts remaining:",attempts)
-        enter_single_char()
-        validate_user_guess(guessed_word, word_to_guess)
+
+        user_guess = enter_single_char()
+        if user_guess in past_guess:
+            print("You already guessed that letter!")
+            continue
+
+        past_guess.append(user_guess)
+        guessed_word,attempts= validate_user_guess(guessed_word, word_to_guess,attempts, user_guess)
+
+    if "_" not in guessed_word or attempts == 0:
+        print("Congratulations! You've successfully guessed the word:",''.join(guessed_word))
+    else:
+        print(f"Game Over! The correct word was {word_to_guess}")
 
 track_past_guesses()
 
